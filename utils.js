523212b948cf0432563964b607183321d4a77f1c
rabby785islam-cmd <rabby785islam@gmail.com>
2026-03-08 08:28:08 +0600

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T02:27:42.549Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T02:28:16.260Z
+Commit: Fix broken links in documentation
